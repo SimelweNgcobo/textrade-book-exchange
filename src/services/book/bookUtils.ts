cadb9839e0c2947bd3592d4e47e1c59a66ae82f0
Simelwe Ngcobo <simelwengcobo@icloud.com>
2025-06-06 10:09:21 +0200

    Deleted QueryClient.tsx

--- src/services/book/bookUtils.ts
+++
@@ -1,9 +0,0 @@
-
-// Commission calculation functions
-export const calculateCommission = (price: number): number => {
-  return price * 0.1; // 10% commission
-};
-
-export const calculateSellerReceives = (price: number): number => {
-  return price - calculateCommission(price);
-};
